638731071e91e4da7eb6b902df3678a43769700d
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-02-08 16:04:02 +0100

    new Icons

--- Design456_Part.py
+++ Design456_Part.py
@@ -147,3 +147,127 @@ class Design456_Part_Cone:
 				'ToolTip':	'Part Cone'
 				}
 Gui.addCommand('Design456_Part_Cone', Design456_Part_Cone())
+
+	
+# Cone
+class Design456_Part_Torus:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Torus","Torus")
+			App.ActiveDocument.ActiveObject.Label = "Torus"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Torus' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Torus.svg',
+				'MenuText': 'Part_Torus',
+				'ToolTip':	'Part Torus'
+				}
+Gui.addCommand('Design456_Part_Torus', Design456_Part_Torus())
+
+# Wedge
+class Design456_Part_Wedge:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Wedge","Wedge")
+			App.ActiveDocument.ActiveObject.Label = "Wedge"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Wedge' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Wedge.svg',
+				'MenuText': 'Part_Wedge',
+				'ToolTip':	'Part Wedge'
+				}
+Gui.addCommand('Design456_Part_Wedge', Design456_Part_Wedge())
+
+
+# Prism
+class Design456_Part_Prism:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Prism","Prism")
+			App.ActiveDocument.ActiveObject.Label = "Prism"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Prism' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Prism.svg',
+				'MenuText': 'Part_Prism',
+				'ToolTip':	'Part Prism'
+				}
+Gui.addCommand('Design456_Part_Prism', Design456_Part_Prism())
+
+
+# Pyramid
+class Design456_Part_Pyramid:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Pyramid","Pyramid")
+			App.ActiveDocument.ActiveObject.Label = "Pyramid"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Pyramid' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Pyramid.svg',
+				'MenuText': 'Part_Pyramid',
+				'ToolTip':	'Part Pyramid'
+				}
+Gui.addCommand('Design456_Part_Pyramid', Design456_Part_Pyramid())
+
+
+# Hemisphere
+class Design456_Part_Hemisphere:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Hemisphere","Hemisphere")
+			App.ActiveDocument.ActiveObject.Label = "Hemisphere"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Hemisphere' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Hemisphere.svg',
+				'MenuText': 'Part_Hemisphere',
+				'ToolTip':	'Part Hemisphere'
+				}
+Gui.addCommand('Design456_Part_Hemisphere', Design456_Part_Hemisphere())
+
+# Ellipsoid
+class Design456_Part_Ellipsoid:
+		
+	def Activated(self):
+		try:
+			App.ActiveDocument.addObject("Part::Ellipsoid","Ellipsoid")
+			App.ActiveDocument.ActiveObject.Label = "Ellipsoid"
+			App.ActiveDocument.recompute()
+			#Gui.SendMsgToActiveView("ViewFit")
+		except ImportError as err:
+			App.Console.PrintError("'Part::Ellipsoid' Failed. "
+								   "{err}\n".format(err=str(err)))
+	def GetResources(self):
+		return {
+				'Pixmap' : Design456Init.ICON_PATH + '/Part_Ellipsoid.svg',
+				'MenuText': 'Part_Ellipsoid',
+				'ToolTip':	'Part Ellipsoid'
+				}
+Gui.addCommand('Design456_Part_Ellipsoid', Design456_Part_Ellipsoid())
