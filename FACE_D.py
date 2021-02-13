781d0e2bb16559e93cf67cb91cdecf3aa30cfe9e
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-02-13 16:03:34 +0100

    Adding move

--- FACE_D.py
+++ FACE_D.py
@@ -90,3 +90,57 @@ class FACE_D:
         except ImportError as err:
             App.Console.PrintError("'FACE_D.ExtractFace' Failed. "
                                    "{err}\n".format(err=str(err)))
+
+
+#This class is from A2P WB modified by Mariwan
+class PartMover:
+    def __init__(self, view, obj):
+        self.obj = obj
+        self.initialPosition = self.obj.Placement.Base
+        self.view = view
+        self.callbackMove = self.view.addEventCallback("SoLocation2Event",self.moveMouse)
+        self.callbackClick = self.view.addEventCallback("SoMouseButtonEvent",self.clickMouse)
+        self.callbackKey = self.view.addEventCallback("SoKeyboardEvent",self.KeyboardEvent)
+        self.objectToDelete = None # object reference when pressing the escape key
+        
+    def moveMouse(self, info):
+        newPos = self.view.getPoint( *info['Position'] )
+        self.obj.Placement.Base = newPos
+        
+    def removeCallbacks(self):
+        self.view.removeEventCallback("SoLocation2Event",self.callbackMove)
+        self.view.removeEventCallback("SoMouseButtonEvent",self.callbackClick)
+        self.view.removeEventCallback("SoKeyboardEvent",self.callbackKey)
+        
+    def clickMouse(self, info):
+        if info['Button'] == 'BUTTON1' and info['State'] == 'DOWN':
+            #if not info['ShiftDown'] and not info['CtrlDown']: #struggles within Inventor Navigation
+            if not info['ShiftDown']:
+                self.removeCallbacks()
+                App.ActiveDocument.recompute()
+            elif info['ShiftDown']:
+                self.obj = duplicateImportedPart(self.obj)
+                self.deleteOnEscape = True
+                
+    def KeyboardEvent(self, info):
+        if info['State'] == 'UP' and info['Key'] == 'ESCAPE':
+            self.removeCallbacks()
+            if not self.deleteOnEscape:
+                self.obj.Placement.Base = self.initialPosition
+            else:
+                self.objectToDelete = self.obj #This can be asked by a timer in a calling func...
+                #This causes a crash in FC0.19/Qt5/Py3             
+                #FreeCAD.activeDocument().removeObject(self.obj.Name)
+#===============================================================================
+toolTip = \
+'''
+Move the selected part.
+
+Select a part and hit this
+button. The part can be moved
+around by mouse.
+
+If the part is constrained, it
+will jump back by next solving
+of the assembly.
+'''
