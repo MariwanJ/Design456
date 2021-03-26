455b4ed6ca18fbd61cc03012e842b4a1d9dc6de8
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-03-26 21:21:01 +0100

    Change constants from function to variable

--- 3DWidgets/fr_window.py
+++ 3DWidgets/fr_window.py
@@ -33,58 +33,15 @@ import pivy.coin as coin
 import init
 import fr_group
 import fr_widget
+import constant
 
-
-"""
-    def mouseEvents(self, mouseEvent):
-        #Address mouse activities.
-        event = mouseEvent.getEvent()
-        if event.getTypeId() == coin.SoLocation2Event.getClassTypeId():
-            FreeCAD.Console.PrintMessage("We are in SoLocation2Event\n")
-        elif event.getTypeId() == coin.SoMouseButtonEvent.getClassTypeId():
-            FreeCAD.Console.PrintMessage("We are in SoMouseButtonEvent\n")
-        else:
-            
-            FreeCAD.Console.PrintMessage("We are in nothing")
-                def mouseEvents(self, mouseEvent):
-
-
-            #Address mouse activities.
-        event = mouseEvent.getEvent()
-        if type(event) == coin.SoMouseButtonEvent:
-            FreeCAD.Console.PrintMessage("We are in SoMouseButtonEvent")
-        elif type(event) == coin.SoLocation2Event:
-            FreeCAD.Console.PrintMessage("We are in SoLocation2Event")
-        else:
-            FreeCAD.Console.PrintMessage("We are in nothing")
-
-
-
-
-class ButtonTest:
-    def __init__(self):
-        self.view = FreeCADGui.ActiveDocument.ActiveView
-        self.callback = self.view.addEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.getMouseClick) 
-
-    def getMouseClick(self, event_cb):
-        event = event_cb.getEvent()
-        if event.getState() == coin.SoMouseButtonEvent.DOWN:
-            print("Alert!!! A mouse button has been improperly clicked!!!")
-            self.view.removeEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.callback)
-
-ButtonTest()
-
-
-
-SoFile.getClassId()
-
-
-"""
 
 class Fr_Window(fr_group.Fr_Group):
     __view=None
-    __event=None
-    __eventXYZ=App.Vertex(0,0,0)       #This should keep the mouse pointer position on the 3D view
+    __lastEvent=None
+    __lastKeyEvent=[]       #Might be more than one key.
+    __lastEventXYZ=App.Vertex(0,0,0)       #This should keep the mouse pointer position on the 3D view
+    
     def __init__(self, x,y,z,h,w,t,l):
         super().__init__(x,y,z,h,w,t,l)
         __view=Gui.ActiveDocument.ActiveView
@@ -93,10 +50,83 @@ class Fr_Window(fr_group.Fr_Group):
     def removeCallbacks(self):
         pass
     def addCallbacks(self):
-        self.callback = self.view.addEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.event_process) 
-        self.callback = self.view.addEventCallbackPivy(coin.SoKeyboardEvent.getClassTypeId(), self.event_process)
+        self.callback = self.view.addEventCallbackPivy(coin.SoMouseButtonEvent.getClassTypeId(), self.event_process)  # Mouse buttons
+        self.callback = self.view.addEventCallbackPivy(coin.SoKeyboardEvent.getClassTypeId(), self.event_process)     # Keyboard 
+        self.view.removeEventCallbackPivy(coin.SoLocation2Event.getClassTypeId(), self.event_process)                # Mouse Move
+        
     #All event come to this function. We classify the event to be processed by each widget later.  
+    
     def event_process(self,events):
-        event = events.getEvent()
         #write down all possible events.
-        if event == self.__event.isKeyPressEvent(event, event.ESC):
+        getEvent = events.getEvent()
+        if (type(getEvent) == coin.SoMouseButtonEvent):
+            if (type(getEvent) == coin.SoMouseButtonEvent and getEvent.getState() == coin.SoMouseButtonEvent.DOWN and getEvent.getButton() == coin.SoMouseButtonEvent.BUTTON1):
+                lastEvent=constant.Fr_Events.MOUSE_LEFT_CLICK()
+            if (type(getEvent) == coin.SoMouseButtonEvent and getEvent.getState() == coin.SoMouseButtonEvent.DOWN and getEvent.getButton() == coin.SoMouseButtonEvent.BUTTON3):
+                lastEvent=constant.Fr_Events.MOUSE_MIDDLE_CLICK()
+            if (type(getEvent) == coin.SoMouseButtonEvent and getEvent.getState() == coin.SoMouseButtonEvent.DOWN and getEvent.getButton() == coin.SoMouseButtonEvent.BUTTON2):
+                lastEvent=constant.Fr_Events.MOUSE_RIGHT_CLICK()
+            
+        elif (type(event) == coin.SoKeyboardEvent):
+            key = ""
+            try:
+                key = event.getKey()
+            except ValueError:
+                # there is no character for this value
+                key = ""
+            
+            if (key == coin.SoKeyboardEvent.LEFT_CONTROL() or coin.SoKeyboardEvent.RIGHT_CONTROL() ) and 
+                    event.getState() == coin.SoButtonEvent.DOWN:
+                __lastKeyEvent.append(constant.FR_EVENTS        
+                
+                    self.snap = True
+                elif event.getState() == coin.SoButtonEvent.UP:
+                    self.snap = False
+            elif key == coin.SoKeyboardEvent.RETURN:
+                self.accept()
+                self.finish()
+            elif key == coin.SoKeyboardEvent.BACKSPACE and event.getState() == coin.SoButtonEvent.UP:
+                self.removePole()
+            elif key == coin.SoKeyboardEvent.I and event.getState() == coin.SoButtonEvent.UP:
+                self.increaseDegree()
+            elif key == coin.SoKeyboardEvent.D and event.getState() == coin.SoButtonEvent.UP:
+                self.decreaseDegree()
+            elif key == coin.SoKeyboardEvent.ESCAPE:
+                self.abort()
+                self.finish()
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        elif (type(event) == coin.SoLocation2Event):
+            pos = App.ActiveDocument.ActiveView.getCursorPos()
+            
+            pnt = self.view.getPoint(pos)
+           FreeCAD.Console.PrintMessage("World coordinates: " + str(pnt) + "\n")
+           info = self.view.getObjectInfo(pos)
+            
+            
+            self.point = self.view.getPoint(pos[0],pos[1])
+            if self.snap:
+                self.getSnapPoint(pos)
+            self.cursorUpdate()
+    ''' 
+        Remove all callbacks registerd for Fr_Window widget
+    '''    
+    def exitFr_Window(self):
+        self.view.removeEventCallbackPivy( coin.SoKeyboSoMouseButtonEvent.getClassTypeId(), self.event_process)
+        self.view.removeEventCallbackPivy( coin.SoKeyboardEvent.getClassTypeId(), self.event_process)
+        self.view.removeEventCallbackPivy( coin.SoLocation2Event.getClassTypeId(), self.event_process)
