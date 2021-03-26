427655b3b1fd1f7c19bcfd9ccd6444199a54e723
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-03-26 22:25:45 +0100

    more code added to fr_window

--- 3DWidgets/constant.py
+++ 3DWidgets/constant.py
@@ -90,7 +90,7 @@ class FR_BoxType: #There will not be any boundary box or bkg box
     FR_TRIANGLE_BOX=        120
     FR_TRIANGLE_FRAME=      130
         
-
+#No constant for keyboard as it is defined already in coin.
 class FR_EVENTS:    
     SKIP_EVENT=               0
     MOUSE_LEFT_CLICK=        10
@@ -99,10 +99,50 @@ class FR_EVENTS:
     MOUSE_ENTER_CLICK=       40
     MOUSE_DRAG=              50
     MOUSE_MOVE=              60
-
-    MOUSE_KEY_UP=            70
-    MOUSE_KEY_DOWN=          80
-    FR_ENTER=                90
-    FR_CTRL=                 100
-    FR_SHIFT=                110
-    FR_ALT=                  120
+    
+    #The enum key definition in COIN3D. We don't want to redefine them. 
+    # to find them use coin.SoKeyboardEvent.(Key name here without brackets)
+    '''
+    enum Key {
+         ANY = 0,
+         UNDEFINED = 1,
+    
+         LEFT_SHIFT = 0xffe1, RIGHT_SHIFT, LEFT_CONTROL, RIGHT_CONTROL,
+         LEFT_ALT = 0xffe9, RIGHT_ALT,
+    
+         NUMBER_0 = 0x0030, NUMBER_1, NUMBER_2, NUMBER_3, NUMBER_4, NUMBER_5,
+         NUMBER_6, NUMBER_7, NUMBER_8, NUMBER_9,
+    
+         A = 0x0061, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T,
+         U, V, W, X, Y, Z,
+    
+         HOME = 0xff50, LEFT_ARROW, UP_ARROW, RIGHT_ARROW, DOWN_ARROW,
+         PAGE_UP, PAGE_DOWN, END,
+         PRIOR = 0xff55, NEXT,
+    
+         PAD_ENTER = 0xff8d,
+         PAD_F1 = 0xff91, PAD_F2, PAD_F3, PAD_F4,
+         PAD_0 = 0xff9e, PAD_1 = 0xff9c, PAD_2 = 0xff99, PAD_3 = 0xff9b,
+         PAD_4 = 0xff96, PAD_5 = 0xff9d, PAD_6 = 0xff98, PAD_7 = 0xff95,
+         PAD_8 = 0xff97, PAD_9 = 0xff9a,
+         PAD_ADD = 0xffab, PAD_SUBTRACT = 0xffad,
+         PAD_MULTIPLY = 0xffaa, PAD_DIVIDE = 0xffaf,
+         PAD_SPACE = 0xff8d, PAD_TAB = 0xff89,
+         PAD_INSERT = 0xff9e, PAD_DELETE = 0xff9f, PAD_PERIOD = 0xff9f,
+    
+         F1 = 0xffbe, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+    
+         BACKSPACE = 0xff08, TAB = 0xff09,
+         RETURN = 0xff0d, ENTER = 0xff0d,
+         PAUSE = 0xff13, SCROLL_LOCK = 0xff14,
+         ESCAPE = 0xff1b, DELETE = 0xffff, KEY_DELETE = DELETE,
+         PRINT = 0xff61, INSERT = 0xff63,
+         NUM_LOCK = 0xff7f, CAPS_LOCK = 0xffe5, SHIFT_LOCK = 0xffe6,
+    
+         SPACE = 0x0020, APOSTROPHE = 0x0027,
+         COMMA = 0x002c, MINUS = 0x002d, PERIOD = 0x002e, SLASH = 0x002f,
+         SEMICOLON = 0x003b, EQUAL = 0x003d,
+         BRACKETLEFT = 0x005b, BACKSLASH = 0x005c,
+         BRACKETRIGHT = 0x005d, GRAVE = 0x0060
+       };
+    '''
