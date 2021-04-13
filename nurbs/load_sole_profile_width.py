b78c87d8bda9ca091d8e0768172ad951d50a7aa7
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-04-13 12:58:04 +0200

    bug fix

--- nurbs/load_sole_profile_width.py
+++ nurbs/load_sole_profile_width.py
@@ -38,11 +38,7 @@ import FreeCADGui
 import Design456Init
 import os
 
-try:
-    import numpy as np 
-except ImportError:
-    print ("Trying to Install required module: numpy")
-    os.system('python -m pip3 install numpy')
+import numpy as np 
 
 spreadsheet_lib
 #reload(.spreadsheet_lib)
