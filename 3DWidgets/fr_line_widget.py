0036a1a849294112fd9bcea2efc0d5efa7b1426f
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-04-27 12:59:34 +0200

    still no progress

--- 3DWidgets/fr_line_widget.py
+++ 3DWidgets/fr_line_widget.py
@@ -134,7 +134,7 @@ class Fr_Line_Widget(fr_widget.Fr_Widget):
             if self.is_visible():
                 linedraw =fr_draw.draw_line(p1, p2, usedColor, self._lineWidth)
                 _lbl=self.draw_label()  
-                self._parent.Root_SeneGraph.addChild(_lbl)
+               # self._parent.Root_SeneGraph.addChild(_lbl)
                 #self._parent.addSoSwitchToSeneGraph(_lbl)
                 # put the node inside the switch
                 self.addSeneNodes(linedraw)  # Add SoSeparator
