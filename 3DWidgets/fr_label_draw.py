209ea56fa001df3754a905e7a98867557b85b986
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-04-27 19:22:15 +0200

    At the end it starting to work

--- 3DWidgets/fr_label_draw.py
+++ 3DWidgets/fr_label_draw.py
@@ -34,107 +34,98 @@ import Design456Init
 from constant import FR_ALIGN
 from constant import FR_COLOR
 
-#todo continue fixing this code 2021-04-23 Mariwan
-def calculateAlignment(vectors,align):
-    p1=p2=p3=p4=None
-    #These variables will keep the min value of each coordinations
-        #WE HAVE LEFT ALIGNMENT
-    p1=vectors[0]
-    p2=vectors[1]
-    FourVector=False
-    if len(vectors==4):
-        FourVector=True
-        p3=vectors[0]
-        p4=vectors[1]
-        
-    minX=minY=minZ=maxX=minY=minZ=None
-    if FourVector==1:
-        minX=min([p1.x,p2.x,p3.x,p4.x])
-        minY=min([p1.y,p2.y,p3.y,p4.y])
-        minZ=min([p1.z,p2.z,p3.z,p4.z])
-    else:
-        minX=min([p1.x,p2.x])
-        minY=min([p1.y,p2.y])
-        minZ=min([p1.z,p2.z])
-    if FourVector:
-        maxX=max([p1.x,p2.x,p3.x,p4.x])
-        maxY=max([p1.y,p2.y,p3.y,p4.y])
-        maxZ=max([p1.z,p2.z,p3.z,p4.z])
-    else:
-        maxX=max([p1.x,p2.x])
-        maxY=max([p1.y,p2.y])
-        maxZ=max([p1.z,p2.z])
+# todo continue fixing this code 2021-04-23 Mariwan
 
-    if len(vectors)<2:
-        return (0,0,0)  #We don't have any vectors, return zero 
+
+def calculateAlignment(vectors, align):
+    p1 = p2 = p3 = p4 = None
+    # These variables will keep the min value of each coordinations
+        # WE HAVE LEFT ALIGNMENT
+    p1 = vectors[0]
+    p2 = vectors[1]
+    FourVector = False
+    if len(vectors == 4):
+        FourVector = True
+        p3 = vectors[0]
+        p4 = vectors[1]
+
+    minX = minY = minZ = maxX = minY = minZ = None
+    if FourVector == 1:
+        minX = min([p1.x, p2.x, p3.x, p4.x])
+        minY = min([p1.y, p2.y, p3.y, p4.y])
+        minZ = min([p1.z, p2.z, p3.z, p4.z])
+    else:
+        minX = min([p1.x, p2.x])
+        minY = min([p1.y, p2.y])
+        minZ = min([p1.z, p2.z])
+    if FourVector:
+        maxX = max([p1.x, p2.x, p3.x, p4.x])
+        maxY = max([p1.y, p2.y, p3.y, p4.y])
+        maxZ = max([p1.z, p2.z, p3.z, p4.z])
+    else:
+        maxX = max([p1.x, p2.x])
+        maxY = max([p1.y, p2.y])
+        maxZ = max([p1.z, p2.z])
+
+    if len(vectors) < 2:
+        return (0, 0, 0)  # We don't have any vectors, return zero
         '''
         The shape is like this
-                                p1___________________ p2  
+                                p1___________________ p2
                                 |                    |
                                 |    face            |
                                 |                    |
                                p3 ___________________ p4
         not having p3,p4 means it is a line only
         '''
-    if align==FR_ALIGN.FR_ALIGN_LEFT or align==FR_ALIGN.FR_ALIGN_LEFT_BOTTOM:
+    if align == FR_ALIGN.FR_ALIGN_LEFT or align == FR_ALIGN.FR_ALIGN_LEFT_BOTTOM:
 
-        
-        
         return (p1)
-        
-    elif align==FR_ALIGN.FR_ALIGN_RIGHT or align==FR_ALIGN.FR_ALIGN_RIGHT_BOTTOM:
-        #WE HAVE RIGHT ALIGNMENT
+
+    elif align == FR_ALIGN.FR_ALIGN_RIGHT or align == FR_ALIGN.FR_ALIGN_RIGHT_BOTTOM:
+        # WE HAVE RIGHT ALIGNMENT
         pass
-    elif align==FR_ALIGN.FR_ALIGN_CENTER or align==FR_ALIGN.FR_ALIGN_CENTER_BOTTOM:
-        #WE HAVE CENTER-BOTTOM ALIGNMENT
+    elif align == FR_ALIGN.FR_ALIGN_CENTER or align == FR_ALIGN.FR_ALIGN_CENTER_BOTTOM:
+        # WE HAVE CENTER-BOTTOM ALIGNMENT
         pass
-    elif align==FR_ALIGN.FR_ALIGN_LEFT_TOP:
-        #Align LEFT-TOP
+    elif align == FR_ALIGN.FR_ALIGN_LEFT_TOP:
+        # Align LEFT-TOP
         pass
-    elif align==FR_ALIGN.FR_ALIGN_RIGHT_TOP:
-        #Align RIGHT-TOP
+    elif align == FR_ALIGN.FR_ALIGN_RIGHT_TOP:
+        # Align RIGHT-TOP
         pass
-    elif align==FR_ALIGN.FR_ALIGN_CENTER_TOP:
-        #Align CENTER-TOP
+    elif align == FR_ALIGN.FR_ALIGN_CENTER_TOP:
+        # Align CENTER-TOP
         pass
-    elif align==FR_ALIGN.FR_ALIGN_CENTER_CENTER:
-        #Align LEFT-TOP
+    elif align == FR_ALIGN.FR_ALIGN_CENTER_CENTER:
+        # Align LEFT-TOP
         pass
-        
-def draw_label(labelcolor=(1, 0, 1), labelfont='sans', size=20,align=FR_ALIGN.FR_NO_ALIGN, trans=App.Vector(0.0, 0.0, 0.0), text=''):
-    # try:
-    global _textNode
-    _transPosition = coin.SoTransform()
-    pos=App.Vector(30,30,0)
-    p=pos
-    _transPosition.translation.setValue(p.x+10,p.y+10,p.z+10)
-    #_transPosition.translation.setValue(trans)
-    font = coin.SoFont()
-    font.size=size                      #Font size   
-    #font.Name=labelfont                 #Font used
-    _text3D = coin.SoAsciiText()           #Draw text in the 3D world 
-    #_text3D.string.setValues(text)         #Actual text
-    _text3D.string.setValues([l.encode("utf8") for l in text if l])
-    print(labelcolor)
-    color=(1,0,0)
-    coinColor = coin.SoMaterial()       #Font color
-    coinColor.diffuseColor.set1Value(0, coin.SbColor(*color))
-    _textNode =coin.SoSeparator()   # A Separator to separate the text from the drawing
-    _textNode.addChild(_transPosition)
-    _textNode.addChild(coinColor)
-    _textNode.addChild(font)
-    _textNode.addChild(_text3D)
-    root=Gui.ActiveDocument.ActiveView.getSceneGraph()
-    newNode=coin.SoSeparator()
-    newNode.addChild(_textNode)
-    root.addChild(newNode)
-    return newNode  # Return the created SoSeparator that contains the text
-    # except Exception as err:
-    #     App.Console.PrintError("'Design456_Extract' Failed. "
-    #                                "{err}\n".format(err=str(err)))
-    #     exc_type, exc_obj, exc_tb = sys.exc_info()
-    #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    #     print(exc_type, fname, exc_tb.tb_lineno)
+
+
+def draw_label(text='', labelcolor=(1, 1, 1), labelfont='sans', size=8, align=FR_ALIGN.FR_NO_ALIGN, trans=App.Vector(0.0, 0.0, 0.0)):
+    try:
+        global _textNode
+        _transPosition = coin.SoTransform()
+        _transPosition.translation.setValue(trans)
+        font = coin.SoFont()
+        font.size = size  # Font size
+        font.Name = labelfont  # Font used
+        _text3D = coin.SoAsciiText()  # Draw text in the 3D world
+        _text3D.string.setValues([l.encode("utf8") for l in text if l])
+        coinColor = coin.SoMaterial()  # Font color
+        coinColor.diffuseColor.set1Value(0, coin.SbColor(*labelcolor))
+        _textNode = coin.SoSeparator()   # A Separator to separate the text from the drawing
+        _textNode.addChild(_transPosition)
+        _textNode.addChild(coinColor)
+        _textNode.addChild(font)
+        _textNode.addChild(_text3D)
+        return _text3D  # Return the created SoSeparator that contains the text
+    except Exception as err:
+        App.Console.PrintError("'draw_label' Failed. "
+                                   "{err}\n".format(err=str(err)))
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
 
 
     """@property
