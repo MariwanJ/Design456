65eb2f2b39fe1035c3c5dbe920824c4d4269db23
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-05-23 22:50:27 +0200

    some code added to smart_scale

--- DirectModeling/smart_scale.py
+++ DirectModeling/smart_scale.py
@@ -39,12 +39,35 @@ from PySide.QtCore import QT_TRANSLATE_NOOP
 
 class Design456_SmartScale:
 
+    def getXYZdimOfSelectedObject(self,selected):
+        #Max object length in all directions
+        lengthX = selected.Shape.BoundBox.XLength
+        lengthY =selected.Shape.BoundBox.YLength
+        lengthY =selected.Shape.BoundBox.ZLength
+        
+        #Make the end 10 mm longer/after the object
+        NewX= selected.Shape.BoundBox.XMax+10
+        NewY= selected.Shape.BoundBox.YMax+10
+        NewZ= selected.Shape.BoundBox.ZMax+10
+        
+        #Make the start 10 mm before the object is placed
+        startX= selected.Shape.BoundBox.XMin-10
+        startY= selected.Shape.BoundBox.YMin-10
+        startZ= selected.Shape.BoundBox.ZMin-10
+
+        
     def Activated(self):
         try:
-            pass 
+            sel = Gui.Selection.getSelection()
+            if len(sel) != 1:
+                # Only one object must be selected
+                errMessage = "Select one object to scale"
+                faced.getInfo().errorDialog(errMessage)
+                return 
         
+        # we have a selected object. Try to show the dimentions. 
         except Exception as err:
-            App.Console.PrintError("'Design456_Extract' Failed. "
+            App.Console.PrintError("'Design456_SmartScale' Failed. "
                                    "{err}\n".format(err=str(err)))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
