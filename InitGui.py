9c43941f1cc17281878fa8468f0a35200ca15fe6
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-05-27 15:38:42 +0200

    some more code in smartscale

--- InitGui.py
+++ InitGui.py
@@ -51,7 +51,7 @@ class Design456_Workbench (Workbench):
         import Design456_2Ddrawing as TwoDDraw
         import Design456_Part_Tools as _tools
         import Design456_SelectionGate as SelGate
-        
+        import DirectModeling.directModelingCommands as dModeling
         # from Part import CommandShapes     #Tube  not working
         Gui.runCommand('Std_PerspectiveCamera', 1)
 
@@ -59,6 +59,7 @@ class Design456_Workbench (Workbench):
         self.appendToolbar("Design456 2Ddrawing",TwoDDraw.Design456_2Ddrawing.list)
         self.appendToolbar("Design456 Tools", _tools.Design456_Part_Tools.list)
         self.appendToolbar("Selection Mode",SelGate.Design456_SelectionGate.list)
+        self.appendToolbar("Selection Mode",dModeling.Design456_DirectModeling.list)
 
 
         self.appendMenu("Design456_Part",designPart.Design456_Part.list)
@@ -135,7 +136,7 @@ class Design456_Workbench (Workbench):
                 "Draft", "Draft modification tools"), self.modification_commands)
             self.appendToolbar(QT_TRANSLATE_NOOP(
                 "Draft", "Draft utility tools"), self.utility_small)
-
+            
             # Set up menus
             self.appendMenu(QT_TRANSLATE_NOOP(
                 "Draft", "&Drafting"), self.drawing_commands)
