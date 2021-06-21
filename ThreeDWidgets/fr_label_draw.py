6c343860f18e91d64f57fd5f660efc31a833b592
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-06-21 23:11:47 +0200

    bug fix

--- ThreeDWidgets/fr_label_draw.py
+++ ThreeDWidgets/fr_label_draw.py
@@ -153,8 +153,8 @@ def draw_label(text=[], prop: propertyValues=None):
         delta=App.Vector(0,0,0)        
         p1=App.Vector(prop.vectors[0])  #You must cast the value or it will fail
         p2=App.Vector(prop.vectors[1])
-        delta.x=p1.x+2
-        delta.y=p1.y+2
+        delta.x=p1.x
+        delta.y=p1.y
         delta.z=p1.z
         (r,thi,phi)=calculateLineSpherical(prop.vectors)        #get spherical representation of the point(p2)
         _transPositionPOS=coin.SoTransform()
