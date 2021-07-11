1cdf3c0c95bb80f0d7de612dfc19a661a2470bb2
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-07-11 12:23:44 +0200

    starting new Design456 fillet

---
+++ DirectModeling/Design456_fillet.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+#
+# ***************************************************************************
+# *                                                                        *
+# * This file is a part of the Open Source Design456 Workbench - FreeCAD.  *
+# *                                                                        *
+# * Copyright (C) 2021                                                     *
+# *                                                                        *
+# *                                                                        *
+# * This library is free software; you can redistribute it and/or          *
+# * modify it under the terms of the GNU Lesser General Public             *
+# * License as published by the Free Software Foundation; either           *
+# * version 2 of the License, or (at your option) any later version.       *
+# *                                                                        *
+# * This library is distributed in the hope that it will be useful,        *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+# * Lesser General Public License for more details.                        *
+# *                                                                        *
+# * You should have received a copy of the GNU Lesser General Public       *
+# * License along with this library; if not, If not, see                   *
+# * <http://www.gnu.org/licenses/>.                                        *
+# *                                                                        *
+# * Author : Mariwan Jalal   mariwan.jalal@gmail.com                       *
+# **************************************************************************
+
+import os,sys
+import FreeCAD as App
+import FreeCADGui as Gui
+import Draft
+import Part
+from pivy import coin
+import FACE_D as faced
+from PySide.QtCore import QT_TRANSLATE_NOOP
+import ThreeDWidgets.fr_line_widget as wlin
+import ThreeDWidgets.fr_coinwindow  as win
+from ThreeDWidgets import fr_coin3d
+from typing import ItemsView, List
+import Design456Init
+from PySide import QtGui,QtCore
+from ThreeDWidgets.fr_arrow_widget import Fr_Arrow_Widget
+import math
+import ThreeDWidgets
+from ThreeDWidgets.constant import FR_EVENTS
+from ThreeDWidgets.constant import FR_COLOR
+from draftutils.translate import translate   #for translate 
+
+Design456_Fillet:
+    
+    
+    
+    
+    
+    def Activated(self):
+            
+
