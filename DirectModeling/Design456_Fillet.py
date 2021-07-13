ed777d1aa5581c929138509aad47ecd4573b9243
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-07-13 16:59:37 +0200

    Update and rename Design456_fillet.py to Design456_Fillet.py
    file name changed

--- DirectModeling/Design456_Fillet.py
+++ DirectModeling/Design456_Fillet.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 #
-# ***************************************************************************
+# **************************************************************************
 # *                                                                        *
 # * This file is a part of the Open Source Design456 Workbench - FreeCAD.  *
 # *                                                                        *
