9ac4bf4b6ebd4252551c3e259acafb960c5ab946
Mariwan Jalal <mariwan.jalal@gmail.com>
2021-08-31 18:43:40 +0200

    some bugs are fixed but there are still works to do

--- DirectModeling/Design456_SmartExtrude.py
+++ DirectModeling/Design456_SmartExtrude.py
@@ -302,7 +302,7 @@ class Design456_SmartExtrude:
 
         rotation = self.getArrowPosition()
         self.directionBasedOnNewVector()
-        self.smartInd = Fr_Arrow_Widget(self._vector, "Extrude", 1, FR_COLOR.FR_RED, rotation, 1)
+        self.smartInd = Fr_Arrow_Widget(self._vector, "Extrude", 1, FR_COLOR.FR_RED, rotation, 2)
         self.smartInd.w_callback_ = callback_release
         self.smartInd.w_move_callback_ = callback_move
         self.smartInd.w_userData.callerObject = self
