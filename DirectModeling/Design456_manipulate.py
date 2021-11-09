6ed59397fbe6036effb38ff6f58e7fc9e49c7df5
Mariwan <mariwan.jalal@gmail.com>
2021-11-09 21:42:54 +0100

    some testing and small bugs are fixed. Several other bugs found

---
+++ DirectModeling/Design456_manipulate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+#
+# ***************************************************************************
+# *                                                                        *
+# * This file is a part of the Open Source Design456 Workbench - FreeCAD.  *
+# *                                                                        *
+# * Copyright (C) 2021                                                     *
+# *                                                                        *
+# *                                                                        *
+# * This library is free software; you can redistribute it and/or          *
+# * modify it under the terms of the GNU Lesser General Public             *
+# * License as published by the Free Software Foundation; either           *
+# * version 2 of the License, or (at your option) any later version.       *
+# *                                                                        *
+# * This library is distributed in the hope that it will be useful,        *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+# * Lesser General Public License for more details.                        *
+# *                                                                        *
+# * You should have received a copy of the GNU Lesser General Public       *
+# * License along with this library; if not, If not, see                   *
+# * <http://www.gnu.org/licenses/>.                                        *
+# *                                                                        *
+# * Author : Mariwan Jalal   mariwan.jalal@gmail.com                       *
+# **************************************************************************
+import os
+import sys
+import FreeCAD as App
+import FreeCADGui as Gui
+import Draft as _draft
+import Part as _part
+import Design456Init
+from pivy import coin
+import math
+from PySide.QtCore import QT_TRANSLATE_NOOP
+from PySide import QtGui, QtCore
+from ThreeDWidgets.constant import FR_BRUSHES
+
+
+
+# >>> import Part
+# >>> __s__=App.ActiveDocument.Compound.Shape.Faces
+# >>> __s__=Part.Solid(Part.Shell(__s__))
+# >>> __o__=App.ActiveDocument.addObject("Part::Feature","Compound_solid")
+# >>> __o__.Label="Compound (Solid)"
+# >>> __o__.Shape=__s__
+# >>> del __s__, __o__
