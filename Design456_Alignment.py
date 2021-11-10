dd609f2ab32edba693b0fb1c67cf2e27d17f8490
Mariwan <mariwan.jalal@gmail.com>
2021-11-10 22:07:25 +0100

    some small changes

--- Design456_Alignment.py
+++ Design456_Alignment.py
@@ -34,24 +34,28 @@ import Draft as _draft
 import Part as _part
 import FACE_D as faced
 import Design456Init
-from draftutils.translate import translate   #for translate
+from draftutils.translate import translate  # for translate
 from pivy import coin
 
 # Toolbar class
-#Based  on https://forum.freecadweb.org/viewtopic.php?style=4&f=22&t=29138&start=20
+# Based  on https://forum.freecadweb.org/viewtopic.php?style=4&f=22&t=29138&start=20
+
+
 class Design456_ViewInsideObjects:
     """
         View internal walls, core of objects in the 3D view
-       
+
     """
+
     def Activated(self):
         try:
             clip_plane = coin.SoClipPlaneManip()
-            clip_plane.setValue(coin.SbBox3f(4, 4, 4, 8, 8, 8), coin.SbVec3f(-1, -1, -1), 1)
+            clip_plane.setValue(coin.SbBox3f(
+                4, 4, 4, 8, 8, 8), coin.SbVec3f(-1, -1, -1), 1)
             Gui.ActiveDocument.ActiveView.getSceneGraph().insertChild(clip_plane, 1)
             Gui.ActiveDocument.ActiveView.viewAxonometric()
             Gui.ActiveDocument.ActiveView.fitAll()
-            
+
         except Exception as err:
             App.Console.PrintError("'View Inside objects' Failed. "
                                    "{err}\n".format(err=str(err)))
@@ -62,32 +66,37 @@ class Design456_ViewInsideObjects:
     def GetResources(self):
         import Design456Init
         return{
-                'Pixmap':    Design456Init.ICON_PATH + 'ViewInsideObjects.svg',
-                'MenuText': 'View Inside objects',
-                'ToolTip':  'View Inside objects'
-            }
+            'Pixmap':    Design456Init.ICON_PATH + 'ViewInsideObjects.svg',
+            'MenuText': 'View Inside objects',
+            'ToolTip':  'View Inside objects'
+        }
 
-Gui.addCommand('Design456_ViewInsideObjects',Design456_ViewInsideObjects())
+
+Gui.addCommand('Design456_ViewInsideObjects', Design456_ViewInsideObjects())
 
 
 """Design456_Alignment"""
 
 # Object Alignment
+
+
 class Design456_AlignFlatToPlane:
+
     def Activated(self):
+        print( Design456Init.DefaultDirectionOfExtrusion," Design456Init.DefaultDirectionOfExtrusion")
         try:
             Selectedobjects = Gui.Selection.getSelectionEx()
-            #This must be modified          
-            newobj=App.ActiveDocument.ActiveObject  #Dummy code.
-            for eachObj in Selectedobjects:
-                if Design456Init.DefaultDirectionOfExtrusion=='z':
-                    eachObj.Object.Placement.Base.z = 0.0
-                elif Design456Init.DefaultDirectionOfExtrusion=='y':
-                    eachObj.Object.Placement.Base.y = 0.0
-                elif Design456Init.DefaultDirectionOfExtrusion=='x':
-                    eachObj.Object.Placement.Base.x = 0.0
-
-            App.ActiveDocument.commitTransaction() #undo reg.     
+            # TODO:This must be modified
+            for obj in Selectedobjects:
+                if Design456Init.DefaultDirectionOfExtrusion == 'z':
+                    obj.Object.Placement.Base.z = 0.0
+                elif Design456Init.DefaultDirectionOfExtrusion == 'y':
+                    obj.Object.Placement.Base.y = 0.0
+                elif Design456Init.DefaultDirectionOfExtrusion == 'x':
+                    obj.Object.Placement.Base.x = 0.0
+            App.ActiveDocument.recompute()
+            
+            App.ActiveDocument.commitTransaction()  # undo reg.
         except Exception as err:
             App.Console.PrintError("'Align to Plain' Failed. "
                                    "{err}\n".format(err=str(err)))
@@ -102,12 +111,15 @@ class Design456_AlignFlatToPlane:
             'MenuText': 'Align To Plane',
             'ToolTip':  'Align to Plane'
         }
-        
-Gui.addCommand('Design456_AlignToPlane',Design456_AlignFlatToPlane())
+
+
+Gui.addCommand('Design456_AlignToPlane', Design456_AlignFlatToPlane())
 
 # Plane Alignments
 
-#Top
+# Top
+
+
 class Design456_TopSideView:
     def Activated(self):
         import Design456Init
@@ -115,7 +127,7 @@ class Design456_TopSideView:
         App.DraftWorkingPlane.alignToPointAndAxis(
             App.Vector(0.0, 0.0, 0.0), App.Vector(0.0, 0.0, 1.0), 0.0)
         Gui.activeDocument().activeView().viewTop()
-        Design456Init.DefaultDirectionOfExtrusion='z'
+        Design456Init.DefaultDirectionOfExtrusion = 'z'
         Gui.Snapper.grid.on()
 
     def GetResources(self):
@@ -124,41 +136,48 @@ class Design456_TopSideView:
             'Pixmap':    Design456Init.ICON_PATH + 'TopSideView.svg',
             'MenuText': 'Top Side View',
             'ToolTip':  'Top Side View'
-            }
+        }
 
-Gui.addCommand('Design456_TopSideView',Design456_TopSideView())
 
-#Bottom View
+Gui.addCommand('Design456_TopSideView', Design456_TopSideView())
+
+# Bottom View
+
+
 class Design456_BottomView:
     import Design456Init
+
     def Activated(self):
         Gui.Snapper.grid.off()
         App.DraftWorkingPlane.alignToPointAndAxis(
             App.Vector(0.0, 0.0, 0.0), App.Vector(0.0, 0.0, -1.0), 0.0)
-        Design456Init.DefaultDirectionOfExtrusion='z'
+        Design456Init.DefaultDirectionOfExtrusion = 'z'
         Gui.activeDocument().activeView().viewBottom()
         Gui.Snapper.grid.on()
         Gui.runCommand("Draft_ToggleGrid")
         Gui.runCommand("Draft_ToggleGrid")
-        
+
     def GetResources(self):
         import Design456Init
         return{
             'Pixmap':    Design456Init.ICON_PATH + 'BottomSideView.svg',
             'MenuText': 'Bottom Side View',
             'ToolTip':  'Bottom Side View'
-            }
+        }
 
-Gui.addCommand('Design456_BottomView',Design456_BottomView())
 
-#Left
+Gui.addCommand('Design456_BottomView', Design456_BottomView())
+
+# Left
+
+
 class Design456_LeftSideView:
     def Activated(self):
         import Design456Init
         Gui.Snapper.grid.off()
         App.DraftWorkingPlane.alignToPointAndAxis(
-            App.Vector(0.0, 0.0, 0.0), App.Vector(1.0,0.0 , 0.0), 0.0)
-        Design456Init.DefaultDirectionOfExtrusion='x'
+            App.Vector(0.0, 0.0, 0.0), App.Vector(1.0, 0.0, 0.0), 0.0)
+        Design456Init.DefaultDirectionOfExtrusion = 'x'
         Gui.activeDocument().activeView().viewLeft()
         Gui.Snapper.grid.on()
 
@@ -168,23 +187,25 @@ class Design456_LeftSideView:
             'Pixmap':    Design456Init.ICON_PATH + 'LeftSideView.svg',
             'MenuText': 'Left Side View',
             'ToolTip':  'Left Side View'
-            }
+        }
 
-Gui.addCommand('Design456_LeftSideView',Design456_LeftSideView())
 
-#Right
+Gui.addCommand('Design456_LeftSideView', Design456_LeftSideView())
+
+# Right
+
+
 class Design456_RightSideView:
     def Activated(self):
         import Design456Init
         Gui.Snapper.grid.off()
         App.DraftWorkingPlane.alignToPointAndAxis(
             App.Vector(0.0, 0.0, 0.0), App.Vector(-1.0, 0.0, 0.0), 0.0)
-        Design456Init.DefaultDirectionOfExtrusion='x'
+        Design456Init.DefaultDirectionOfExtrusion = 'x'
         Gui.activeDocument().activeView().viewRight()
         Gui.Snapper.grid.on()
         Gui.runCommand("Draft_ToggleGrid")
         Gui.runCommand("Draft_ToggleGrid")
-        
 
     def GetResources(self):
         import Design456Init
@@ -192,18 +213,21 @@ class Design456_RightSideView:
             'Pixmap':    Design456Init.ICON_PATH + 'RightSideView.svg',
             'MenuText': 'Right Side View',
             'ToolTip':  'Right Side View'
-            }
+        }
 
-Gui.addCommand('Design456_RightSideView',Design456_RightSideView())
 
-#Front
+Gui.addCommand('Design456_RightSideView', Design456_RightSideView())
+
+# Front
+
+
 class Design456_FrontSideView:
     def Activated(self):
         import Design456Init
         Gui.Snapper.grid.off()
         App.DraftWorkingPlane.alignToPointAndAxis(
-            App.Vector(0.0, 0.0, 0.0), App.Vector(0.0,1.0, 0.0), 0.0)
-        Design456Init.DefaultDirectionOfExtrusion='y'
+            App.Vector(0.0, 0.0, 0.0), App.Vector(0.0, 1.0, 0.0), 0.0)
+        Design456Init.DefaultDirectionOfExtrusion = 'y'
         Gui.activeDocument().activeView().viewFront()
         Gui.Snapper.grid.on()
         Gui.runCommand("Draft_ToggleGrid")
@@ -215,18 +239,22 @@ class Design456_FrontSideView:
             'Pixmap':    Design456Init.ICON_PATH + 'FrontSideView.svg',
             'MenuText': 'Front Side View',
             'ToolTip':  'Front Side View'
-            }
+        }
 
-Gui.addCommand('Design456_FrontSideView',Design456_FrontSideView())
 
-#Back
+Gui.addCommand('Design456_FrontSideView', Design456_FrontSideView())
+
+# Back
+
+
 class Design456_BackSideView:
     import Design456Init
+
     def Activated(self):
         Gui.Snapper.grid.off()
         App.DraftWorkingPlane.alignToPointAndAxis(
             App.Vector(0.0, 0.0, 0.0), App.Vector(0.0, -1.0, 0.0), 0.0)
-        Design456Init.DefaultDirectionOfExtrusion='y'
+        Design456Init.DefaultDirectionOfExtrusion = 'y'
         Gui.activeDocument().activeView().viewRear()
         Gui.Snapper.grid.on()
         Gui.runCommand("Draft_ToggleGrid")
@@ -238,58 +266,65 @@ class Design456_BackSideView:
             'Pixmap':    Design456Init.ICON_PATH + 'BackSideView.svg',
             'MenuText': 'Backside View',
             'ToolTip':  'BackSide View'
-            }
-
-Gui.addCommand('Design456_BackSideView',Design456_BackSideView())
+        }
 
 
-#Design456 Move 
+Gui.addCommand('Design456_BackSideView', Design456_BackSideView())
+
+
+# Design456 Move
 class Design456_MoveObject:
-    
-    def Activated(self): 
+
+    def Activated(self):
         try:
-            #Gui.runCommand('Std_Transform')
+            # Gui.runCommand('Std_Transform')
             Gui.runCommand('Std_TransformManip')
-            return    
-        
+            return
+
         except Exception as err:
             App.Console.PrintError("'Design456_loftOnDirection' Failed. "
                                    "{err}\n".format(err=str(err)))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno) 
-                              
+            print(exc_type, fname, exc_tb.tb_lineno)
+
     def GetResources(self):
         import Design456Init
         return{
             'Pixmap':    Design456Init.ICON_PATH + 'Design456_Move.svg',
             'MenuText': 'Design456 Move',
             'ToolTip':  'Design456 Move'
-            }
+        }
+
+
 Gui.addCommand('Design456_MoveObject', Design456_MoveObject())
 
-#Design456 Move Detailed
+# Design456 Move Detailed
+
+
 class Design456_MoveObjectDetailed:
-    
-    def Activated(self): 
+
+    def Activated(self):
         try:
             Gui.runCommand('Std_Transform')
-            return    
-        
+            return
+
         except Exception as err:
             App.Console.PrintError("'Design456_loftOnDirection' Failed. "
                                    "{err}\n".format(err=str(err)))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno) 
-                              
+            print(exc_type, fname, exc_tb.tb_lineno)
+
     def GetResources(self):
         import Design456Init
         return{
             'Pixmap':    Design456Init.ICON_PATH + 'Design456_MoveD.svg',
             'MenuText': 'Design456 Move-Detailed',
             'ToolTip':  'Design456 Move Detailed'
-            }
+        }
+
+
 Gui.addCommand('Design456_MoveObjectDetailed', Design456_MoveObjectDetailed())
 
 
@@ -297,14 +332,15 @@ class Design456_AlignmentGroup:
 
     """Design456 Part Alignment Tools"""
     #Separator = QtGui.QSpacerItem(5, 237, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+
     def __init__(self):
         return
-    
+
     def GetCommands(self):
         """3D Alignments Tools."""
         return ("Design456_MoveObject",
                 "Design456_MoveObjectDetailed",
-                #"Separator",
+                # "Separator",
                 "Design456_AlignToPlane",
 
                 )
@@ -314,7 +350,7 @@ class Design456_AlignmentGroup:
         from PySide.QtCore import QT_TRANSLATE_NOOP
         """Set icon, menu and tooltip."""
         _tooltip = ("Different Tools for Aligning 3D/2D Shapes")
-        return {'Pixmap':  Design456Init.ICON_PATH+ 'Design456_Alignment.svg',
+        return {'Pixmap':  Design456Init.ICON_PATH + 'Design456_Alignment.svg',
                 'MenuText': QT_TRANSLATE_NOOP("Design456", "Alignment"),
                 'ToolTip': QT_TRANSLATE_NOOP("Design456", _tooltip)}
 
