0b5b8c58de7194711239c10bf0676e664182cf1e
Mariwan <mariwan.jalal@gmail.com>
2022-01-17 21:45:16 +0100

    adding some more code and fixing moving place of the group 2d tools down in the file

--- Design456_2Ddrawing.py
+++ Design456_2Ddrawing.py
@@ -40,40 +40,11 @@ import Design456_Paint
 import Design456_Hole
 from draftutils.translate import translate  # for translation
 
-__updated__ = '2022-01-17 17:16:11'
+__updated__ = '2022-01-17 21:44:07'
 
 # Move an object to the location of the mouse click on another surface
 
 
-class Design456_2Ddrawing:
-    list = ["Design456_Arc3Points",
-            "Design456_MultiPointsToWireOpen",
-            "Design456_MultiPointsToWireClose",
-            "Design456_2DTrim",
-            "Design456_joinTwoLines",
-            "Design456_Star",
-            "Design456_Paint",
-            "Design456_Hole",
-
-
-            ]
-    """Design456 Design456_2Ddrawing Toolbar"""
-
-    def GetResources(self):
-        return{
-            'Pixmap':    Design456Init.ICON_PATH + '2D_Drawing.svg',
-            'MenuText': '2Ddrawing',
-            'ToolTip':  '2Ddrawing'
-        }
-
-    def IsActive(self):
-        if App.ActiveDocument is None:
-            return False
-        else:
-            return True
-
-    def Activated(self):
-        self.appendToolbar("Design456_2Ddrawing", self.list)
 
 # ***************************************************************************
 # * Modified by: Mariwan Jalal    mariwan.jalal@gmail.com    04/03/2021     *
@@ -649,5 +620,83 @@ class Design456_joinTwoLines:
                 'MenuText': QT_TRANSLATE_NOOP("Design456", "joinTwoLines"),
                 'ToolTip': QT_TRANSLATE_NOOP("Design456", _tooltip)}
 
-
 Gui.addCommand('Design456_joinTwoLines', Design456_joinTwoLines())
+
+
+class Design456_SimplifyEdges:
+    def __init__(self):
+        self.points = []
+    def Activated(self):
+        try:
+
+            self.points.clear()
+            s = Gui.Selection.getSelectionEx()
+            if len(s)>1:
+                #TODO: FIXME: Should we accept more than one object?
+                errMessage = "Select edges from one object"
+                faced.errorDialog(errMessage)
+                return
+            selObj = s[0]
+            selEdges= selObj.Shape.OrderedEdges
+            selVertexs=[]
+            for e in selEdges:
+                selVertexs.append(e.Point)
+            # To eliminate multiple vertices
+            # incide the same edge, we take only
+            # first and last entities and then
+            # we make a line and convert it to 
+            # an edge. which should replace the
+            # old edge 
+
+        except Exception as err:
+            App.Console.PrintError("'Part Surface' Failed. "
+                                   "{err}\n".format(err=str(err)))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+    def GetResources(self):
+        import Design456Init
+        from PySide.QtCore import QT_TRANSLATE_NOOP
+        """Set icon, menu and tooltip."""
+        _tooltip = ("Simplify Edges")
+        return {'Pixmap':  Design456Init.ICON_PATH + 'SimplifyEdge.svg',
+                'MenuText': QT_TRANSLATE_NOOP("Design456", "SimplifyEdges"),
+                'ToolTip': QT_TRANSLATE_NOOP("Design456", _tooltip)}
+
+
+Gui.addCommand('Design456_SimplifyEdges', Design456_SimplifyEdges())
+
+
+##################################################################################
+#       Toolbar group definition 
+class Design456_2Ddrawing:
+    list = ["Design456_Arc3Points",
+            "Design456_MultiPointsToWireOpen",
+            "Design456_MultiPointsToWireClose",
+            "Design456_2DTrim",
+            "Design456_joinTwoLines",
+            "Design456_SimplifyEdges",
+            "Design456_Star",
+            "Design456_Paint",
+            "Design456_Hole",
+
+
+            ]
+    """Design456 Design456_2Ddrawing Toolbar"""
+
+    def GetResources(self):
+        return{
+            'Pixmap':    Design456Init.ICON_PATH + '2D_Drawing.svg',
+            'MenuText': '2Ddrawing',
+            'ToolTip':  '2Ddrawing'
+        }
+
+    def IsActive(self):
+        if App.ActiveDocument is None:
+            return False
+        else:
+            return True
+
+    def Activated(self):
+        self.appendToolbar("Design456_2Ddrawing", self.list)
