c80c23511ac77c372498536b3ac425ff604fe58a
Mariwan <mariwan.jalal@gmail.com>
2022-01-24 22:05:52 +0100

    experimental code - try to fix extrude

--- Design456_Extrude.py
+++ Design456_Extrude.py
@@ -37,113 +37,54 @@ from time import time as _time, sleep as _sleep
 from draftutils.translate import translate  # for translation
 import math
 
-__updated__ = '2022-01-24 18:25:28'
+__updated__ = '2022-01-24 22:04:31'
 
 class Design456_Extrude:
     
-    def ExtractThenExtrude(self):
+    def ExtractFace(self):
         try:
-
-            s = Gui.Selection.getSelectionEx()
-            if (len(s) < 1):
-                # An object must be selected
-                errMessage = "Select an object to use Extrude Face"
-                faced.errorDialog(errMessage)
-                return
-            lengthForward = -QtGui.QInputDialog.getDouble(
-                None, "Length of Extrusion", "Input:", 0, -10000.0, 10000.0, 2)[0]
-            if(lengthForward ==0):
-                return  # nothing to do here
-
-            objName = s[0].ObjectName
-            App.ActiveDocument.openTransaction(translate("Design456","Extrude a face"))
-            sh = s[0].Object.Shape.copy()
-            if hasattr(s[0].Object, "getGlobalPlacement"):
-                gpl = s[0].Object.getGlobalPlacement()
-                sh.Placement = gpl
-            name = s[0].SubElementNames[0]
-            fullname = objName+"_"+name  # Name of the face extracted
+            sh = self.selectedObj[0].SubObjects[0].copy()
+            fullname = "ExtractedFace"
             newobj = s[0].Document.addObject("Part::Feature", fullname)
-            newobj.Shape = sh.getElement(name)
-            selection = newobj  # The face extracted
-            activeSel = Gui.Selection.getSelection(App.ActiveDocument.Name)
-            Gui.Selection.removeSelection(activeSel[0])
-            # Select the face extracted before
-            Gui.Selection.addSelection(selection)
+            newobj.Shape = sh
             App.ActiveDocument.recompute()
-            m = App.ActiveDocument.getObject(fullname)
-            f = App.ActiveDocument.addObject(
-                'Part::Extrusion', 'ExtrudeFace')     # Add extrusion
-            f.Base = newobj             # App.ActiveDocument.getObject(fullname)
-            f.DirMode = "Normal"
-            f.DirLink = None
-            f.LengthFwd = lengthForward
-            f.LengthRev = 0.0
-            f.Solid = True
-            f.Reversed = False
-            f.Symmetric = False
-            f.TaperAngle = 0.0
-            f.TaperAngleRev = 0.0
-            yL = newobj.Shape.Faces[0].CenterOfMass
-            uv = newobj.Shape.Faces[0].Surface.parameter(yL)
-            nv = newobj.Shape.Faces[0].normalAt(uv[0], uv[1])
-            direction = yL.sub(nv + yL)
-            direction= App.Vector(round(direction.x,2),round(direction.y,2),round(direction.z,2) )
-            print(direction,"direction" )
-            r = App.Rotation(App.Vector(0, 0, 0), direction)
-            if (abs(f.Dir.x) != 1 or abs(f.Dir.y) != 1 or abs(f.Dir.z) != 1):
-                f.DirMode = "Custom"
-            f.Dir=direction
-            App.ActiveDocument.recompute()
-            newPart_ = App.ActiveDocument.addObject(
-                'Part::Feature', f.Name+'N')
-            newPart_.Shape = Part.getShape(
-                f, '', needSubElement=False, refine=False)
-            if newPart_.isValid() is False:
-                App.ActiveDocument.removeObject(newPart_.Name)
-                App.ActiveDocument.removeObject(f.Name)
-                # Shape != OK
-                errMessage = "Failed to extrude the Face"
-                faced.getInfo(m).errorDialog(errMessage)
-            else:
-                # remove old objects
-                App.ActiveDocument.removeObject(f.Name)
-                App.ActiveDocument.removeObject(m.Name)
-                # This will not work for split objects as they are not two separate objects.2021-02-22
-                # Make the two objects merged
-                obj1 = App.ActiveDocument.getObject(s[0].ObjectName)
-                obj2 = newPart_
-                selections = Gui.Selection.getSelectionEx()
-                for i in selections:
-                    Gui.Selection.removeSelection(selections)
-                    Gui.Selection.addSelection(obj1)
-                    Gui.Selection.addSelection(obj2)
-                    Gui.runCommand('Design456_Part_Merge', 0)
-            App.ActiveDocument.commitTransaction() #undo reg.
-            App.ActiveDocument.recompute()
+            return newObj # Extracted Face. 
 
-            return
         except Exception as err:
             App.Console.PrintError("'ExtrudeFace' Failed. "
                                    "{err}\n".format(err=str(err)))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
-
-    def Activated(self):
+    def Activated(self):            
         try:
-            selection = Gui.Selection.getSelectionEx()
-            if (len(selection) < 1):
+            self.selectedObj = Gui.Selection.getSelectionEx()
+            if (len(self.selectedObj) < 1):
                 # An object must be selected
                 errMessage = "Select a face to use Extrude"
                 faced.errorDialog(errMessage)
                 return
-            if not isinstance(selection[0].Object.Shape, Part.Face):
-                self.ExtractThenExtrude()
+            AllObjects=None
+            if len(self.selectedObj) == 1:
+                #It is only one object. But might have different faces
+                if not(self.selectedObj[0].HasSubObjects):
+                    #Only One object that doesn't have subobjects
+                    if not isinstance(self.selectedObj[0].Object.Shape, Part.Face):
+                        AllObjects=[self.ExtractThenExtrude()]
+                    else: 
+                        # Here it can be a face with multiple faces
+                        # Or a 3D object with multiple faces?
+                        AllObjects=[self.selectedObj[0].Object]
+                else:
+                    AllObjects=self.selectedObj.SubObjects
             else:
+                allObjects=self.selectedObj
+
+    def ExtrudeFace(self, allObjects):
+        try:
                 App.ActiveDocument.openTransaction(
                     translate("Design456", "Extrude"))
-                m = selection[0].Object
+                m = self.selectedObj[0].Object
                 f = App.ActiveDocument.addObject(
                     'Part::Extrusion', 'ExtrudeOriginal')
                 f.Base = m
