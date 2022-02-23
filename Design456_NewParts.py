0c3fb3933db2adf247212e55e23651b6186efd54
Mariwan Jalal <mariwan.jalal@gmail.com>
2022-02-23 13:00:04 +0100

    some more code for the housing

--- Design456_NewParts.py
+++ Design456_NewParts.py
@@ -138,3 +138,116 @@ class Design456_Seg_Roof:
         faced.PartMover(v, newObj, deleteOnEscape=True)
 
 Gui.addCommand('Design456_Seg_Roof', Design456_Seg_Roof())
+
+
+#***************************
+
+
+
+#Housing
+
+class ViewProviderHousing:
+
+    obj_name = "Housing"
+
+    def __init__(self, obj, obj_name):
+        self.obj_name = ViewProviderHousing.obj_name
+        obj.Proxy = self
+
+    def attach(self, obj):
+        return
+
+    def updateData(self, fp, prop):
+        return
+
+    def getDisplayModes(self, obj):
+        return "As Is"
+
+    def getDefaultDisplayMode(self):
+        return "As Is"
+
+    def setDisplayMode(self, mode):
+        return "As Is"
+
+    def onChanged(self, vobj, prop):
+        pass
+
+    def getIcon(self):
+        return ( Design456Init.ICON_PATH + 'Housing.svg')
+
+    def __getstate__(self):
+        return None
+
+    def __setstate__(self, state):
+        return None
+
+
+#Housing 
+class Design456_HousingBase:
+    """ Housing shape based on several parameters
+    """
+    def __init__(self, obj, 
+                       width=20,
+                       length=20,
+                       height=10,
+                       thickness=1):
+
+
+        obj.addProperty("App::PropertyLength", "Width","Housing", 
+                        "Width of the Housing").Width = width
+
+        obj.addProperty("App::PropertyLength", "Length","Housing", 
+                        "Length of the Housing").Length = length
+
+        obj.addProperty("App::PropertyLength", "Height","Housing", 
+                        "Height of the Housing").Height = height
+
+        obj.addProperty("App::PropertyLength", "Thickness","Housing", 
+                        "Thickness of the Housing").Thickness = thickness
+        obj.Proxy = self
+    
+    def execute(self, obj):
+        self.Width=float(obj.Width)
+        self.Height=float(obj.Height)
+        self.Length=float(obj.Length)
+        self.Thickness=float(obj.Thickness)
+        Result=None
+        V1_FSQ=[App.Vector(0,0,0),
+                 App.Vector(self.Width,0,0),
+                 App.Vector(self.Width,self.Length,0),
+                 App.Vector(0.0,self.Length,0),
+                 App.Vector(0,0,0)]
+
+        V2_FSQ=[App.Vector(self.Thickness,self.Thickness,0),
+                 App.Vector(self.Width-self.Thickness,self.Thickness,0),
+                 App.Vector(self.Width-self.Thickness,self.Length-self.Thickness,0),
+                 App.Vector(self.Thickness,self.Length-self.Thickness,0),
+                 App.Vector(self.Thickness,self.Thickness,0)]
+        firstFace1=Part.Face(Part.Polygon(V1_FSQ))  # one used with secondFace to cut
+        firstFace2=Part.Face(Part.Polygon(V1_FSQ))  # Other used to make the bottom
+        secondFace=Part.Face(Part.Polygon(V2_FSQ))
+        resultButtom=firstFace1.cut(secondFace)
+        extrude1=resultButtom.extrude(0,0,self.Height)
+        extrude2=firstFace2.extrude(0,0,self.Height)
+        result=Part.Merge(extrude1,extrude2)
+        
+        obj.Shape=Result
+        
+class Design456_Housing:
+    def GetResources(self):
+        return {'Pixmap':Design456Init.ICON_PATH + 'Housing.svg',
+                'MenuText': "Housing",
+                'ToolTip': "Generate a Housing"}
+
+    def Activated(self):
+        newObj = App.ActiveDocument.addObject(
+            "Part::FeaturePython", "Housing")
+        Design456_HousingBase(newObj)
+
+        ViewProviderHousing(newObj.ViewObject, "Housing")
+
+        App.ActiveDocument.recompute()
+        v = Gui.ActiveDocument.ActiveView
+        faced.PartMover(v, newObj, deleteOnEscape=True)
+
+Gui.addCommand('Design456_Housing', Design456_Housing())
