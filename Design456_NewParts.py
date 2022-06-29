98a58c23bbd8f74f6fa3f5987d2df62a0e69fe3d
Mariwan Jalal <mariwan.jalal@gmail.com>
2022-06-29 12:57:26 +0200

    bug fix

--- Design456_NewParts.py
+++ Design456_NewParts.py
@@ -1299,7 +1299,7 @@ class Design456_FlowerVase:
         plc=App.Placement()
         plc.Base=App.Vector(0,0,0)
         plc.Rotation.Q = (0.0, 0.0, 0.0, 1.0)
-        newObj.placement=plc
+        newObj.Placement=plc
         Design456_BaseFlowerVase(newObj)
 
         ViewProviderFlowerVase(newObj.ViewObject, "FlowerVase")
